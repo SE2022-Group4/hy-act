5e67537ddb091a8122ce93c090c8c1399c7a94b4
Jongheon Kim <simba3447@gmail.com>
2022-05-25 16:53:38 +0900

    Add API Endpoint for applying program

--- program/views.py
+++ program/views.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets, permissions
+from django import views
+from django.http import Http404
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from program.models import Program
 from program.serializers import ProgramSerializer
@@ -8,3 +12,17 @@ class ProgramViewSet(viewsets.ModelViewSet):
     queryset = Program.objects.order_by('-created_at')
     serializer_class = ProgramSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class ProgramApplyView(APIView):
+    def get_object(self, pk):
+        try:
+            return Program.objects.get(pk=pk)
+        except Program.DoesNotExist:
+            raise Http404
+
+    def post(self, request, pk, format=None):
+        program = self.get_object(pk)
+        program.apply(request.user)
+
+        return Response(status=status.HTTP_200_OK)
