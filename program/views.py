86cb7a14f77beec0fe4e297464db56880d766236
Jongheon Kim <simba3447@gmail.com>
2022-05-26 16:14:27 +0900

    Add filter and search parameter

--- program/views.py
+++ program/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter
 
 from program.models import Program
 from program.serializers import ProgramSerializer
@@ -7,4 +9,7 @@ from program.serializers import ProgramSerializer
 class ProgramViewSet(viewsets.ModelViewSet):
     queryset = Program.objects.order_by('-created_at')
     serializer_class = ProgramSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['name']
+    filterset_fields = ['target_major']
     permission_classes = [permissions.IsAuthenticated]
